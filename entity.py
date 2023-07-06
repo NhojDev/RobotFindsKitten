6da6d93ff42ae50939255e2fcebd29213b8b1afd
NhojDev <johnduypham@gmail.com>
2023-07-06 11:14:00 -0700

    Added Description to Entities

--- entity.py
+++ entity.py
@@ -5,11 +5,12 @@ class Entity:
     """
     A generic object to represent players, enemies, items, etc.
     """
-    def __init__(self, x: int, y: int, char: str, color: Tuple[int, int, int]):
+    def __init__(self, x: int, y: int, char: str, color: Tuple[int, int, int], description: str):
         self.x = x
         self.y = y
         self.char = char
         self.color = color
+        self.description = description
 
     def move(self, dx: int, dy: int) -> None:
         # Move the entity by a given amount
